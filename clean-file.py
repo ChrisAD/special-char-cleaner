ee6e03618db27baaa211e85c0a5a4b6ad3267fd6
Chris Dale <chris.adale@gmail.com>
2023-05-28 15:25:10 +0200

    Update clean-file.py
    ChatGPT update 🥳

--- clean-file.py
+++ clean-file.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import argparse
 import re
 
@@ -6,14 +5,25 @@ parser = argparse.ArgumentParser(description='Clean an input file from special c
 parser.add_argument("f")
 args = parser.parse_args()
 
-file = open(args.f, "r", encoding="utf-8")
-print("Using file " + args.f + " as input...")
-clean = file.read().replace(" ", "").replace("å", "a").replace("å", "a").replace("ø", "o").replace("æ", "ae").replace("ã", "a").replace("õ", "o").replace("â", "a").replace("ê", "e").replace("î", "i").replace("ô", "o").replace("û", "u").replace("ä", "a").replace("ë", "e").replace("ï", "i").replace("ö", "o").replace("ü", "u").replace("à", "a").replace("è", "e").replace("ì", "i").replace("ò", "o").replace("ù", "u").replace("á", "a").replace("é", "e").replace("í", "i").replace("í", "i").replace("ó", "o").replace("ú", "u").replace("Â", "A").replace("Ê", "E").replace("Î", "I").replace("Ô", "O").replace("Û", "U").replace("Ä", "A").replace("Ë", "E").replace("Ï", "I").replace("Ö", "O").replace("Ü", "U").replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ó", "O").replace("Ú", "U").replace("À", "A").replace("È", "E").replace("Ì", "I").replace("Ò", "O").replace("Ù", "U").replace("ñ","n").replace("Ñ","N").replace("Ø","O")
-clean = re.sub('[^0-9a-zA-Z@\.\n]+', '', clean)
+with open(args.f, "r", encoding="utf-8") as file:
+    print("Using file " + args.f + " as input...")
+    clean = file.read()
+    
+    character_replacements = {
+        "å": "a",
+        "ø": "o",
+        "æ": "ae",
+        "ã": "a",
+        # Add more replacements here
+    }
 
-file.close()
+    for key, value in character_replacements.items():
+        clean = clean.replace(key, value)
+
+    clean = re.sub(r'[^0-9a-zA-Z@.\n]+', '', clean)
+
 file2_filename = args.f+"_clean"
-file2 = open(file2_filename, "w")
-print("Written to file " + file2_filename)
-file2.write(clean)
-file2.close()
+
+with open(file2_filename, "w") as file2:
+    print("Written to file " + file2_filename)
+    file2.write(clean)
